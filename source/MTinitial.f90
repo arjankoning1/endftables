48e1296b5f9d7c78784bb0b93f68bf12dc4ed718
Arjan Koning <arjankoning1@gmail.com>
2025-12-15 22:45:10 +0100

    version

--- source/MTinitial.f90
+++ source/MTinitial.f90
@@ -22,7 +22,7 @@ subroutine MTinitial(MT)
 !
 ! *** Initialization
 !
-  oformat='YANDF-0.2'
+  oformat='YANDF-0.4'
   mass='000'
   write(mass(1:3),'(i3.3)') Atarget
   endfhead = proj//'-'//trim(El)//mass
